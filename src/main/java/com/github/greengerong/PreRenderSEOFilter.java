77fe1149d30cbf0f49effd512e238a37a9a96009
greengerongg <grzx2210@163.com>
2014-02-15 17:26:11 +0800

    refactor

--- src/main/java/com/github/greengerong/PreRenderSEOFilter.java
+++ src/main/java/com/github/greengerong/PreRenderSEOFilter.java
@@ -2,17 +2,12 @@ package com.github.greengerong;
 
 
 import com.google.common.base.Predicate;
-import com.google.common.collect.Lists;
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.*;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIUtils;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.message.HeaderGroup;
 import org.slf4j.Logger;
@@ -27,7 +22,6 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -37,43 +31,16 @@ import static com.google.common.collect.FluentIterable.from;
 public class PreRenderSEOFilter implements Filter {
 
     private final static Logger log = LoggerFactory.getLogger(PreRenderSEOFilter.class);
-
-    private FilterConfig filterConfig;
-
     private CloseableHttpClient httpClient;
     private PreRenderEventHandler preRenderEventHandler;
+    private PrerenderConfig prerenderConfig;
+    public static final int HTTP_OK = 200;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        this.filterConfig = filterConfig;
-        this.httpClient = getHttpClient(filterConfig);
-        this.preRenderEventHandler = getEventHandler(filterConfig);
-    }
-
-    private PreRenderEventHandler getEventHandler(FilterConfig filterConfig) {
-        final String preRenderEventHandler = filterConfig.getInitParameter("preRenderEventHandler");
-        if (StringUtils.isNotBlank(preRenderEventHandler)) {
-            try {
-                return (PreRenderEventHandler) Class.forName(preRenderEventHandler).newInstance();
-            } catch (Exception e) {
-                log.error("PreRenderEventHandler class not find or can not new a instance", e);
-            }
-        }
-        return null;
-    }
-
-    private CloseableHttpClient getHttpClient(FilterConfig filterConfig) {
-        HttpClientBuilder builder = HttpClients.custom();
-
-        final String proxy = filterConfig.getInitParameter("proxy");
-        if (StringUtils.isNotBlank(proxy)) {
-            final int proxyPort = Integer.parseInt(filterConfig.getInitParameter("proxyPort"));
-            DefaultProxyRoutePlanner routePlanner = new DefaultProxyRoutePlanner(new HttpHost(proxy, proxyPort));
-            builder = builder.setRoutePlanner(routePlanner);
-        }
-
-        builder = builder.setConnectionManager(new PoolingHttpClientConnectionManager());
-        return builder.build();
+        this.prerenderConfig = new PrerenderConfig(filterConfig);
+        this.httpClient = prerenderConfig.getHttpClient();
+        this.preRenderEventHandler = prerenderConfig.getEventHandler();
     }
 
     @Override
@@ -115,7 +82,7 @@ public class PreRenderSEOFilter implements Filter {
         withPrerenderToken(getMethod);
         CloseableHttpResponse proxyResponse = httpClient.execute(getMethod);
         try {
-            if (proxyResponse.getStatusLine().getStatusCode() == 200) {
+            if (proxyResponse.getStatusLine().getStatusCode() == HTTP_OK) {
                 afterRender(request, proxyResponse);
                 copyResponseHeaders(proxyResponse, response);
                 copyResponseEntity(proxyResponse, response);
@@ -134,7 +101,7 @@ public class PreRenderSEOFilter implements Filter {
     }
 
     private void withPrerenderToken(HttpRequest proxyRequest) {
-        final String token = filterConfig.getInitParameter("prerenderToken");
+        final String token = prerenderConfig.getPrerenderToken();
         //for new version prerender with token.
         if (StringUtils.isNotBlank(token)) {
             proxyRequest.addHeader("X-Prerender-Token", token);
@@ -169,7 +136,9 @@ public class PreRenderSEOFilter implements Filter {
 
     protected void closeQuietly(Closeable closeable) {
         try {
-            closeable.close();
+            if (closeable != null) {
+                closeable.close();
+            }
         } catch (IOException e) {
             log.error("Close proxy error", e);
         }
@@ -212,7 +181,7 @@ public class PreRenderSEOFilter implements Filter {
                     // rewrite the Host header to ensure that we get content from
                     // the correct virtual server
                     if (headerName.equalsIgnoreCase(HttpHeaders.HOST)) {
-                        HttpHost host = URIUtils.extractHost(new URI(getPrerenderServiceUrl()));
+                        HttpHost host = URIUtils.extractHost(new URI(prerenderConfig.getPrerenderServiceUrl()));
                         headerValue = host.getHostName();
                         if (host.getPort() != -1) {
                             headerValue += ":" + host.getPort();
@@ -236,52 +205,8 @@ public class PreRenderSEOFilter implements Filter {
 
     @Override
     public void destroy() {
-        filterConfig = null;
-        try {
-            httpClient.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private List<String> getCrawlerUserAgents() {
-        List<String> crawlerUserAgents = Lists.newArrayList("googlebot", "yahoo", "bingbot", "baiduspider",
-                "facebookexternalhit", "twitterbot", "rogerbot", "linkedinbot", "embedly");
-        final String crawlerUserAgentsFromConfig = filterConfig.getInitParameter("crawlerUserAgents");
-        if (StringUtils.isNotBlank(crawlerUserAgentsFromConfig)) {
-            crawlerUserAgents.addAll(Arrays.asList(crawlerUserAgentsFromConfig.trim().split(",")));
-        }
-
-        return crawlerUserAgents;
-    }
-
-    private List<String> getExtensionsToIgnore() {
-        List<String> extensionsToIgnore = Lists.newArrayList(".js", ".css", ".less", ".png", ".jpg", ".jpeg",
-                ".gif", ".pdf", ".doc", ".txt", ".zip", ".mp3", ".rar", ".exe", ".wmv", ".doc", ".avi", ".ppt", ".mpg",
-                ".mpeg", ".tif", ".wav", ".mov", ".psd", ".ai", ".xls", ".mp4", ".m4a", ".swf", ".dat", ".dmg",
-                ".iso", ".flv", ".m4v", ".torrent");
-        final String extensionsToIgnoreFromConfig = filterConfig.getInitParameter("extensionsToIgnore");
-        if (StringUtils.isNotBlank(extensionsToIgnoreFromConfig)) {
-            extensionsToIgnore.addAll(Arrays.asList(extensionsToIgnoreFromConfig.trim().split(",")));
-        }
-
-        return extensionsToIgnore;
-    }
-
-    private List<String> getWhitelist() {
-        final String whitelist = filterConfig.getInitParameter("whitelist");
-        if (StringUtils.isNotBlank(whitelist)) {
-            return Arrays.asList(whitelist.trim().split(","));
-        }
-        return null;
-    }
-
-    private List<String> getBlacklist() {
-        final String blacklist = filterConfig.getInitParameter("blacklist");
-        if (StringUtils.isNotBlank(blacklist)) {
-            return Arrays.asList(blacklist.trim().split(","));
-        }
-        return null;
+        prerenderConfig = null;
+        closeQuietly(httpClient);
     }
 
     private boolean shouldShowPrerenderedPage(HttpServletRequest request) throws URISyntaxException {
@@ -316,13 +241,13 @@ public class PreRenderSEOFilter implements Filter {
             return false;
         }
 
-        final List<String> whiteList = getWhitelist();
+        final List<String> whiteList = prerenderConfig.getWhitelist();
         if (whiteList != null && !isInWhiteList(url, whiteList)) {
             log.trace("Whitelist is enabled, but this request is not listed; intercept: no");
             return false;
         }
 
-        final List<String> blacklist = getBlacklist();
+        final List<String> blacklist = prerenderConfig.getBlacklist();
         if (blacklist != null && isInBlackList(url, referer, blacklist)) {
             log.trace("Blacklist is enabled, and this request is listed; intercept: no");
             return false;
@@ -337,16 +262,11 @@ public class PreRenderSEOFilter implements Filter {
     }
 
     private String getApiUrl(String url) {
-        String prerenderServiceUrl = getPrerenderServiceUrl();
+        String prerenderServiceUrl = prerenderConfig.getPrerenderServiceUrl();
         if (!prerenderServiceUrl.endsWith("/")) {
             prerenderServiceUrl += "/";
         }
         return prerenderServiceUrl + url;
-    }
-
-    private String getPrerenderServiceUrl() {
-        final String prerenderServiceUrl = filterConfig.getInitParameter("prerenderServiceUrl");
-        return StringUtils.isNotBlank(prerenderServiceUrl) ? prerenderServiceUrl : "'http://service.prerender.io/'";
     }
 
     private boolean isInBlackList(final String url, final String referer, List<String> blacklist) {
@@ -370,7 +290,7 @@ public class PreRenderSEOFilter implements Filter {
     }
 
     private boolean isInResources(final String url) {
-        return from(getExtensionsToIgnore()).anyMatch(new Predicate<String>() {
+        return from(prerenderConfig.getExtensionsToIgnore()).anyMatch(new Predicate<String>() {
             @Override
             public boolean apply(String item) {
                 return url.contains(item.toLowerCase());
@@ -379,7 +299,7 @@ public class PreRenderSEOFilter implements Filter {
     }
 
     private boolean isInSearchUserAgent(final String userAgent) {
-        return from(getCrawlerUserAgents()).anyMatch(new Predicate<String>() {
+        return from(prerenderConfig.getCrawlerUserAgents()).anyMatch(new Predicate<String>() {
             @Override
             public boolean apply(String item) {
                 return userAgent.toLowerCase().indexOf(item.toLowerCase()) >= 0;
